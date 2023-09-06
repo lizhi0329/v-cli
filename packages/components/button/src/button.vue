007e35bc9aee3dbe6557831408231b927a4437e4
lizhi02 <lizhi02@rd.netease.com>
2023-09-06 14:47:13 +0800

    init

---
+++ packages/components/button/src/button.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { hello } from '@luckin/shared';
+
+const props = withDefaults(defineProps<{
+  text?: string;
+}>(), {
+  text: 'World',
+});
+
+function clickHandler() {
+  hello(props.text);
+}
+</script>
+
+<template>
+  <button
+    class="openx-button"
+    @click="clickHandler"
+  >
+    1<slot />
+  </button>
+</template>
+
---
+++ packages/components/shared/src/index.ts
@@ -0,0 +1,3 @@
+// packages/shared/src/index.ts
+export * from './test';
+export * from './useLodash';
